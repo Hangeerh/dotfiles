5bff187f1e9b28e054bf5980b2eeb268fd1a7656
hangeer <hangeerh@sina.com>
2026-04-07 13:01:06 -0500

    Ghostty and nvim qol
      * Make ghostty background more opaque
    
      * Neovim markdown hard line wrap

--- .config/nvim/lua/core/autocommands.lua
+++ .config/nvim/lua/core/autocommands.lua
@@ -11,3 +11,11 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     vim.bo.filetype = "objc"
   end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.textwidth = 80
+    vim.opt_local.formatoptions:append("t")
+  end,
+})
