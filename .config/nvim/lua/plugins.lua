7c51fdc4acb70c90d27e047a67e35f41ae2a0947
hangeer <hangeerh@sina.com>
2026-04-29 16:02:28 -0500

    Remove nastiness for basedpyright

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -145,6 +145,23 @@ vim.lsp.config("lua_ls", {
     Lua = {},
   },
 })
+
+vim.lsp.config("basedpyright", {
+  settings = {
+    basedpyright = {
+      analysis = {
+        autoSearchPaths = true,
+        diagnosticMode = "openFilesOnly",
+        typeCheckingMode = "basic",
+        -- https://docs.basedpyright.com/latest/configuration/language-server-settings/
+        -- Explicitly setting `basedpyright.analysis.useLibraryCodeForTypes` is **discouraged** by the official docs.
+        -- Because it will override per-project configurations like `pyproject.toml`.
+        -- If left unset, its default value is `true`, and it can be correctly overridden by project config files.
+      },
+    },
+  },
+})
+
 -- Overrides end here
 require("mason-lspconfig").setup({
   automatic_enable = false,
